694089689c4714dccbecf0fb13859ebeab3601bf
Abhishek Raj <137596383+abhishek20202020@users.noreply.github.com>
2025-07-28 20:55:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UpcomingReservations from './components/UpcomingReservations';
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <UpcomingReservations />
+    </div>
+  );
+}
+
+export default App;
